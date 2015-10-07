f934460230631f5b949b7c85139c65fdfc2c6267
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-10-06 20:12:13 -0400

    added csv file and input/output file and modified code

---
+++ ISBN_input_output.rb
@@ -0,0 +1,22 @@
+require_relative "valid_ISBN.rb"
+
+def create_output_file
+    file_original = File.open('input_isbn_file.csv', "r")
+    file_new = File.open('isbn_output.csv', "w")
+    
+    file_original.each do |row|
+      valid_ISBN(row)
+        if @output == true
+          file_new.puts row.chomp + ", valid"
+        else
+          file_new.puts row.chomp + ", invalid"
+        end
+    
+    end     
+
+	file_original.close
+    file_new.close
+    
+end
+
+create_output_file 
--- valid_ISBN.rb
+++ valid_ISBN.rb
@@ -5,17 +5,48 @@ def valid_ISBN(number)
 		  number.delete! ' '
 		elsif number.include? "-"
 		  number.delete! '-'
+		else
+		number
 		end
-		
+		remove_extra_characters(number)
 		number.length == 10 || number.length == 13
 end
 
+def remove_extra_characters(number)
+    number_array = number.split ("")
+    number_array.slice!(0)
+    number_array.slice!(-1)
+    number_array.slice!(-1)
+    character_length(number_array)
+end
+
+def character_length(number)
+    @number_array = number    
+	if
+      @number_array.length == 10
+       checkdigit_contains_X(@number_array)
+        if only_numeric_characters(@new_number) == true
+        @output = false    
+        else ten_character_math(@new_number)
+        end
+    elsif
+      @number_array.length == 13
+        if only_numeric_characters(@number_array) == true
+        @output = false
+        else thirteen_character_math(@number_array)
+        end
+    else
+      @output =  false
+    end 
+end
 def checkdigit_contains_X(number)
-	number_array = number.split ""
-		if number_array[9] == "X" || number_array[9] == "x"
-		  number_array[9] = 10
+	
+		if number[9] == "X" || number[9] == "x"
+		  number[9] = 10
 		end
-	   number_array[9] == 10
+	   @new_number = number
+	   number[9] == 10
+	   
 end
 
 def only_numeric_characters(number)
@@ -29,23 +60,36 @@ end
 	 
 
 def ten_character_math(any_array)
+	array = []
+	  any_array.each do |value|
+      array << value.to_i
+    end
  	sum = 0
- 		if any_array.count == 10
-		  any_array.each_with_index do |value, index_position|
+ 		
+		  array.each_with_index do |value, index_position|
  		  break if index_position == 9
 		  sum += value * (index_position + 1)
 		end
-		end
-		any_array[9] == sum % 11
+		check_digit = sum % 11
+		if array[9] == check_digit
+          @output = true
+        else
+          @output = false
+        end 
+		
 end
 
 	
 def thirteen_character_math(any_array)
+	array = []
+	  any_array.each do |value|
+      array << value.to_i
+    end
  	sum = 0
 	check_digit = 0
-		if any_array.count == 13
+		
 	
-		any_array.each_with_index do |value, index_position|
+		array.each_with_index do |value, index_position|
 		
 		break if index_position == 12
 		
@@ -60,8 +104,12 @@ def thirteen_character_math(any_array)
 			
 			if check_digit == 10
 				check_digit = 0
-			end			  
+			end		
+		if array[12] == check_digit
+            @output = true
+        else
+            @output = false
+        end 	
 		
 	end
-		any_array[12] == check_digit
-end
+		
